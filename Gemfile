a08ad2ca88ceda25d3c8e4cc219f01aee6a1356e
andresporras3423 <andresporras3423@gmail.com>
2020-03-04 15:17:26 -0500

    stickle offenses corrected

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'geocoder'
 gem 'openweather2'
 gem 'telegram/bot'
-gem 'geocoder'
--- chatbot.rb
+++ chatbot.rb
@@ -3,47 +3,51 @@ class Chatbot
   attr_accessor :interval, :no_more, :chosen_format
   def initialize(options)
     @options = options
-    @interval=60
-    @no_more=false
-    @chosen_format="C"
+    @interval = 60
+    @no_more = false
+    @chosen_format = 'C'
   end
 
   def celsius_weather(weather)
-    return nil if weather==nil
+    return nil if weather.nil?
 
-    if chosen_format=='C'
-      celsius= (weather.temperature-273.15).round(2)
+    if chosen_format == 'C'
+      celsius = (weather.temperature - 273.15).round(2)
       return "#{celsius}°C"
-    elsif chosen_format=='F'
-      farenheit= (((weather.temperature-273.15)*1.8)+32).round(2)
+    elsif chosen_format == 'F'
+      farenheit = (((weather.temperature - 273.15) * 1.8) + 32).round(2)
       return "#{farenheit}°F"
     end
-    return "#{weather.temperature}°K"
+    "#{weather.temperature}°K"
   end
-  
+
   def give_bot_message(temperature, place, error_message)
     temperature_message = yield place, temperature
-    return temperature.nil? ? error_message : temperature_message
+    temperature.nil? ? error_message : temperature_message
   end
 
   def update_format(message)
     temp_format = message.text.downcase.gsub('/format', '').gsub(/\s+/m, '')
-    if temp_format=~/\A[kfc]{1}\Z/ 
+    if temp_format =~ /\A[kfc]{1}\Z/
       @chosen_format = temp_format.upcase
       return "new format is °#{chosen_format}"
     end
-    return "invalid format"
+    'invalid format'
   end
 
   def update_interval(message)
     temp_interval = message.text.downcase.gsub('/interval', '').gsub(/\s+/m, '')
     if temp_interval =~ /\A[1-9]+[0-9]*[smhd]{1}\Z/
-      amount = temp_interval[0..(temp_interval.length-2)].to_i
-      chosen_option = options.find{|opt| opt.time==temp_interval[temp_interval.length-1]}
-      chosen_format_time = amount==1 ? chosen_option.format_time[0..(chosen_option.format_time.length-2)] : chosen_option.format_time
-      @interval= amount*chosen_option.number
+      amount = temp_interval[0..(temp_interval.length - 2)].to_i
+      chosen_option = options.find { |opt| opt.time == temp_interval[temp_interval.length - 1] }
+      chosen_format_time = if amount == 1
+                             chosen_option.format_time[0..(chosen_option.format_time.length - 2)]
+                           else
+                             chosen_option.format_time
+                           end
+      @interval = amount * chosen_option.number
       return "new interval in #{amount} #{chosen_format_time}"
     end
-    return "invalid interval"
+    'invalid interval'
   end
 end
