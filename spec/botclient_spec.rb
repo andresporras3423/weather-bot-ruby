93e2a8855bdc7f1fdcd38a1d6f7f14cbb66c4a68
andresporras3423 <andresporras3423@gmail.com>
2020-03-06 07:28:18 -0500

    correcting stickler offenses

--- spec/botclient_spec.rb
+++ spec/botclient_spec.rb
@@ -1,6 +1,5 @@
-require_relative '../lib/botclient'
-require_relative '../lib/chatbot'
 require_relative '../lib/test_classes'
+require_relative '../lib/botclient'
 
 token_telegram = '1109360723:AAHam4xsAf-7wgF8Hjt6ACbxxOH66cimbaM'
 key_weather = 'a71219e79a6b01978ac3a9f3ffccca37'
@@ -19,7 +18,7 @@ RSpec.describe Botclient do
       (expect do
         botclient.capture_error(['miami'], 'weather_city')
       end
-      ).to output("").to_stdout
+      ).to output('').to_stdout
     end
   end
   describe 'tests method capture_error' do
@@ -56,55 +55,6 @@ RSpec.describe Botclient do
     end
     it 'test when weather_city and valid params' do
       expect { botclient.capture_error(['london'], 'weather_city') }.to output('').to_stdout
-    end
-  end
-end
-
-RSpec.describe Chatbot do
-  let(:botclient) { Botclient.new(token_telegram, key_weather, false) }
-  describe 'tests method celsius_weather' do
-    it 'test when param is nil' do
-      expect(botclient.chatbot.celsius_weather(nil)).to eql(nil)
-    end
-    it 'test when passed a valid parameter' do
-      expect(botclient.chatbot.celsius_weather(Weather.new(293.15))).to eql('20.0°C')
-    end
-    it 'test when passed a valid parameter and format is F' do
-      botclient.chatbot.chosen_format = 'F'
-      expect(botclient.chatbot.celsius_weather(Weather.new(293.15))).to eql('68.0°F')
-    end
-    it 'test when passed a valid parameter and format is K' do
-      botclient.chatbot.chosen_format = 'K'
-      expect(botclient.chatbot.celsius_weather(Weather.new(293.15))).to eql('293.15°K')
-    end
-  end
-  describe 'tests method give_bot_message' do
-    it 'test when param temperature is nil' do
-      expect(botclient.chatbot.give_bot_message(nil, 'bogotá', 'invalid data') do |x, y|
-        "temperature in #{x} is #{y}"
-      end).to eql('invalid data')
-    end
-    it 'test when param temperature is valid' do
-      expect(botclient.chatbot.give_bot_message('16°C', 'bogotá', 'invalid data') do |x, y|
-        "temperature in #{x} is #{y}"
-      end).to eql('temperature in bogotá is 16°C')
-    end
-  end
-  describe 'tests method update_format' do
-    it 'test when format is not valid' do
-      expect(botclient.chatbot.update_format(Msg.new("/format #{Regexp.last_match(1124)}"))).to eql('invalid format')
-    end
-    it 'test when format is valid' do
-      expect(botclient.chatbot.update_format(Msg.new('/format f'))).to eql('new format is °F')
-    end
-  end
-  describe 'tests method update_interval' do
-    it 'test when interval is not valid' do
-      msg = Msg.new("/interval #{Regexp.last_match(1124)}")
-      expect(botclient.chatbot.update_interval(msg)).to eql('invalid interval')
-    end
-    it 'test when interval is valid' do
-      expect(botclient.chatbot.update_interval(Msg.new('/interval 2m'))).to eql('new interval in 2 minutes')
     end
   end
 end
